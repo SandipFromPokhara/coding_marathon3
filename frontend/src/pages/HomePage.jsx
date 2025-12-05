85905eea26e080494130b3460749a8721432e626
Olga <olga.chitembo@metropolia.fi>
2025-12-05 11:20:42 +0200

    add features: edit job, home page and routing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,36 @@
 import JobListings from "../components/JobListings";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [jobs, setJobs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("/api/jobs");
+        if (!res.ok) {
+          throw new Error("could not fetch the data for that resource");
+        }
+        const data = await res.json();
+        setIsPending(false);
+        setJobs(data);
+        setError(null);
+      } catch (err) {
+        setIsPending(false);
+        setError(err.message);
+      }
+    };
+    
+    fetchJobs();
+  }, []);
+
   return (
     <div className="home">
-      <JobListings  />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {jobs && <JobListings jobs={jobs} />}
     </div>
   );
 };
