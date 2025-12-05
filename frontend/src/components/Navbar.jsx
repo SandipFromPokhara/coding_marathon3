74b1730c54d03839d8d98017b7b5af90e0776b9f
Olga <olga.chitembo@metropolia.fi>
2025-12-05 13:50:20 +0200

    added signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-const Navbar = () => {
+import { Link } from "react-router-dom";
+
+const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+  const handleLogout = () => {
+  setIsAuthenticated(false);
+  localStorage.removeItem("user");
+  console.log("User logged out");
+};
+
   return (
     <nav className="navbar">
       <h1>Job Search</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/add-job">Add Job</a>
+        <Link to="/">Home</Link>
+        {isAuthenticated && <Link to="/add-job">Add Job</Link>}
+        {!isAuthenticated && <Link to="/signup">Sign Up</Link>}
+        {isAuthenticated && <button onClick={handleLogout}>Logout</button>}
       </div>
     </nav>
   );
