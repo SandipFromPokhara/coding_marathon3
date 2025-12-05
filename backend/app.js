e65fcf82bb60beb56eb129378139fd2e1c10be27
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-05 11:54:49 +0200

    Remove user route for no-auth

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const jobRouter = require("./routes/jobRouter");
-const userRouter = require("./routes/userRouter");
+// const userRouter = require("./routes/userRouter");
 const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -16,7 +16,7 @@ connectDB();
  
 // Use the jobRouter for all "/jobs" routes
 app.use("/api/jobs", jobRouter);
-app.use("/api/users", userRouter);
+// app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
